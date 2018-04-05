989f627b598aef1afce1a5c489898ecb3c417bd4
msshriner <markusshriner@gmail.com>
2018-04-05 00:50:48 -0700

    class work

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,11 +2,11 @@
 
 # Create a vector `first_ten` that has the values 10 through 20 in it (using 
 # the : operator)
-
+first_ten <- 10:20
 
 # Create a vector `next_ten` that has the values 21 through 30 in it (using the 
 # seq() function)
-
+next_ten <- seq(21, 30)
 
 # Create a vector `all_numbers` by combining the previous two vectors
 
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,7 +2,7 @@
 
 # Create a vector `words` of 6 (or more) words.
 # You can Google for a "random word generator" if you wish!
-
+words <- c("Apple", "Banana", "Citrus", "Dragonfruit", "Eggfruit", "Fig")
 
 # Create a vector `words_of_the_day` that is your `words` vector with the string
 # "is the word of the day!" pasted on to the end.
